a9ab6d992b6db5ae3c2485cc2a0250a9e74aaaa7
Ryan Baker <ryan.da.baker@gmail.com>
2015-03-13 19:12:35 -0400

    adds anothony's email

--- config/initializers/approved_users.rb
+++ config/initializers/approved_users.rb
@@ -3,5 +3,6 @@ Rails.application.config.approved_users = [
   'ryan.da.baker@gmail.com',
   'shrinkandb@gmail.com',
   'kzizza@gmail.com',
-  'tylerstonephoto@gmail.com'
+  'tylerstonephoto@gmail.com',
+  'Amrusso351@gmail.com'
 ]
