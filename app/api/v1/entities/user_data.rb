796caa253eb6826121b3afa9e8960405dd2ad0a9
Ryan Baker <ryan.da.baker@gmail.com>
2015-04-10 12:37:36 -0400

    updates the entities to be propertly represented in the docs

--- app/api/v1/entities/user_data.rb
+++ app/api/v1/entities/user_data.rb
@@ -1,14 +1,13 @@
 module V1::Entities
   class UserData
-    class AsPrivate < Grape::Entity
+    class AsPublic < Grape::Entity
       expose :first_name, documentation: { type: "String", desc: "The user's first name." }
       expose :last_name, documentation: { type: "String", desc: "The user's last name." }
-      expose :email, documentation: { type: "String", desc: "The user's email." }
       expose :id, documentation: { type: "Integer", desc: "The user's id." }
     end
 
-    class AsPublic < AsPrivate
-      unexpose :email
+    class AsPrivate < AsPublic
+      expose :email, documentation: { type: "String", desc: "The user's email." }
     end
   end
 end
