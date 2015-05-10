e5ce26f90f9e844ba0a2692c42cd578e3f81b1d0
Tyler Stone <tyler.stone@emc.com>
2015-05-10 12:18:50 -0400

    added some code back to application.html, removed bower components and added cdn links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,2 @@
-//= require angular/angular.min
-//= require angular-ui-utils/ui-utils.min
-//= require angular-ui-router/release/angular-ui-router.min
-//= require angular-bootstrap/ui-bootstrap-tpls.min
 //= require angular-rails-templates
 //= require_tree .
