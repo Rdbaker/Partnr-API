c560f220887b3aaf7705e452566831a3a40db413
Ryan Baker <ryan.da.baker@gmail.com>
2015-10-08 08:57:38 -0400

    Adds test kitchen's (exec only) emails to the whitelist.

--- config/initializers/approved_users.rb
+++ config/initializers/approved_users.rb
@@ -5,5 +5,11 @@ Rails.application.config.approved_users = [
   'kzizza@gmail.com',
   'tylerstonephoto@gmail.com',
   'amrusso351@gmail.com',
-  'vinny.sabo@gmail.com'
+  'vinny.sabo@gmail.com',
+  'veronicam.delaney@gmail.com',
+  'v.e.booth001@gmail.com',
+  'marc@mandtsolutions.com',
+  'cabel999@gmail.com',
+  'derekfeehrer@gmail.com',
+  'xell13@gmail.com'
 ]
