02f385980a28877fe1cfdeb3466c0781fc8873e7
Tyler Stone <tyler.stone@emc.com>
2015-10-30 23:24:45 -0400

    added project tile visuals

--- app/assets/javascripts/projects/listProjectCtrl.js
+++ app/assets/javascripts/projects/listProjectCtrl.js
@@ -2,6 +2,16 @@ angular.module('partnr.users.assets').controller('ListProjectController', functi
 	$scope.projects = [];
 	$scope.loadComplete = false;
 	
+	$scope.getProjectStatus = function(status) {
+		if (status === 'not_started') {
+			return "Not Started";
+		} else if (status === 'in_progress') {
+			return "In Progress";
+		} else {
+			return "Complete";
+		}
+	}
+
 	projects.list().then(function(result) {
 		$scope.projects = result.data;
 		$scope.loadComplete = true;
