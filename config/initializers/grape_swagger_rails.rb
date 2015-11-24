f5895ce190b8e95f9979de8e2ff731bcfa1156c9
Ryan Baker <ryan.da.baker@gmail.com>
2015-11-23 22:45:02 -0500

    Corrects the API docs URL specified for swagger.

--- config/initializers/grape_swagger_rails.rb
+++ config/initializers/grape_swagger_rails.rb
@@ -1,4 +1,4 @@
-GrapeSwaggerRails.options.url      = 'api/swagger_doc'
+GrapeSwaggerRails.options.url      = 'api/v1/swagger_doc'
 GrapeSwaggerRails.options.app_url  = '/'
 GrapeSwaggerRails.options.app_name = 'Partnr'
 
