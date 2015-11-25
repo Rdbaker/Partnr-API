26f64880f2763d9d7da0c3505258e08e87d31e73
Tyler Stone <tjs_rocks@yahoo.com>
2015-11-25 18:29:45 -0500

    notifications are read upon close of the dropdown

--- app/assets/javascripts/notifications/notificationsDropdownDirective.js
+++ app/assets/javascripts/notifications/notificationsDropdownDirective.js
@@ -1,12 +1,36 @@
-angular.module('partnr.notify').directive('notificationsDropdown', function($rootScope, routeUtils, notifications) {
+angular.module('partnr.notify').directive('notificationsDropdown', function($rootScope, $log, routeUtils, notifications) {
     return {
         restrict: 'AE',
         templateUrl: 'notifications/notifications_dropdown.html',
+        scope: {
+            visible: '='
+        },
         link: function($scope, elem, attr, ctrl) {
+            $scope.dropdownLimit = 10;
             $scope.notifications = notifications.get();
+            $scope.hasBeenOpened = false;
 
             $scope.$on('notifications', function(event, notifications) {
                 $scope.notifications = notifications;
+            });
+
+            $scope.$watch('visible', function() {
+                if ($scope.visible) {
+                    $scope.hasBeenOpened = true;
+                }
+
+                if (!$scope.visible && $scope.hasBeenOpened) {
+                    $log.debug('setting read');
+                    $scope.readNotifications = angular.copy($scope.notifications);
+
+                    var totalRead = ($scope.readNotifications.length > $scope.dropdownLimit 
+                        ? $scope.dropdownLimit : $scope.readNotifications.length);
+                    for (var i = 0; i < totalRead; i++) {
+                        if ($scope.readNotifications[i].read == false) {
+                            notifications.setRead($scope.readNotifications[i].id);
+                        }
+                    }
+                }
             });
         }
     };
