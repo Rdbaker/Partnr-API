ad56143a43f7e461de8f14bbaf72230f1394c03f
Ryan Baker <ryan.da.baker@gmail.com>
2016-01-18 09:15:01 -0500

    Adds the first round of users to the whitelist

--- config/initializers/approved_users.rb
+++ config/initializers/approved_users.rb
@@ -11,5 +11,14 @@ Rails.application.config.approved_users = [
   'marc@mandtsolutions.com',
   'cabel999@gmail.com',
   'derekfeehrer@gmail.com',
-  'xell13@gmail.com'
+  'xell13@gmail.com',
+  'zxu3@wpi.edu',
+  'gmabrogan@gmail.com',
+  'jrquigley@wpi.edu',
+  'nicolelizabeth.baker@gmail.com',
+  'nabaker@wpi.edu',
+  'bradleach6@gmail.com',
+  'frlee@wpi.edu',
+  'gerardbaker@charter.net',
+  'n.kotts@mac.com'
 ]
