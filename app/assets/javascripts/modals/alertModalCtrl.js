c8eef8ac4784096e591d1f989a8ca4b559b1094f
Ryan Baker <ryan.da.baker@gmail.com>
2016-01-26 15:01:53 -0500

    Fixes issues with the modal service.

--- app/assets/javascripts/modals/alertModalCtrl.js
+++ app/assets/javascripts/modals/alertModalCtrl.js
@@ -1,8 +1,8 @@
-angular.module('partnr.notify').controller('AlertModalController', function($scope, $modalInstance, $sce, title, message) {
+angular.module('partnr.notify').controller('AlertModalController', function($scope, $uibModalInstance, $sce, title, message) {
     $scope.title = title;
     $scope.message = $sce.trustAsHtml(message);
     
     $scope.ok = function () {
-        $modalInstance.close('ok');
+        $uibModalInstance.close('ok');
     };
 });
