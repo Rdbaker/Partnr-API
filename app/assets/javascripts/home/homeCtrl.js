7d0ab7b3166589f6cb1b7377b7b04d5e4ad99928
Tyler Stone <tjs_rocks@yahoo.com>
2016-01-26 00:01:26 -0500

    header adjustments... more work to do

--- app/assets/javascripts/home/homeCtrl.js
+++ app/assets/javascripts/home/homeCtrl.js
@@ -1,8 +1,24 @@
 angular.module('partnr.core').controller('HomeController', function($scope, $state, $q, $log,
-	principal, search, toaster) {
-	
+	principal, search, toaster, projects) {
 	$scope.user = principal.getUser();
 	$scope.search = search.createNew();
+	$scope.projects = [];
+	$scope.loadComplete = false;
+	
+	$scope.getProjectStatus = function(status) {
+		if (status === 'not_started') {
+			return "Not Started";
+		} else if (status === 'in_progress') {
+			return "In Progress";
+		} else {
+			return "Complete";
+		}
+	}
+
+	projects.list().then(function(result) {
+		$scope.projects = result.data;
+		$scope.loadComplete = true;
+	});
 
 	$scope.doLogout = function() {
 		principal.logout().then(function() {
