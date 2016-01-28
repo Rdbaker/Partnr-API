fe1a73c9ad8211b277b75045629c827092323c00
Ryan Baker <ryan.da.baker@gmail.com>
2016-01-28 00:04:00 -0500

    Bumps the version to 1.0.3

--- spec/config/application_spec.rb
+++ spec/config/application_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe Partnr::Application, 'configuration' do
       expect(subject.version().class).to eq(String)
     end
 
-    it "is v1.0.0" do
-      expect(subject.version).to eq("1.0.0")
+    it "is v1.0.3" do
+      expect(subject.version).to eq("1.0.3")
     end
   end
 end
