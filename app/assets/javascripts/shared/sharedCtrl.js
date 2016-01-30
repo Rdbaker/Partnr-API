235e1235354884eff05f52d6055ac1a8ae362713
Tyler Stone <tjs_rocks@yahoo.com>
2016-01-30 00:45:43 -0500

    eh, notifications needs a little more work

--- app/assets/javascripts/shared/sharedCtrl.js
+++ app/assets/javascripts/shared/sharedCtrl.js
@@ -1,8 +1,10 @@
 angular.module('partnr.core').controller('SharedController', function($scope, $state, $stateParams, $log, $q, notifications, routeUtils) {
 	$scope.newNotifications = {};
+	$scope.allNotifications = {};
     $scope.notificationsDropdownVisible = false;
 
-    $scope.$on('notifications', function() {
+    $scope.$on('notifications', function(event, updatedNotifications) {
+    	$scope.allNotifications = updatedNotifications;
         $scope.newNotifications = notifications.getNew();
     });
 
