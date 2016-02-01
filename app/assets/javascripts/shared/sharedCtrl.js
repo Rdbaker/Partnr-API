948d959a0b9ab6053a8e5acecc348482a4c22f5f
Tyler Stone <tjs_rocks@yahoo.com>
2016-01-31 22:42:29 -0500

    notification dropdown sends read requests when closed

--- app/assets/javascripts/shared/sharedCtrl.js
+++ app/assets/javascripts/shared/sharedCtrl.js
@@ -17,7 +17,21 @@ angular.module('partnr.core').controller('SharedController', function($scope, $s
         $state.go('profile', { id : principal.getUser().id });
     };
 
-     $scope.resolveLink = function(n) {
+    $scope.resolveLink = function(n) {
         routeUtils.resolveEntityLinkAndGo(n.links.notifier, n, notifications.linkParamResolveStrategy);
+    };
+
+    $scope.setRead = function(open) {
+        if (!open) {
+            $log.debug('setting read');
+            $scope.readNotifications = angular.copy($scope.allNotifications);
+
+            var totalRead = ($scope.readNotifications.length > 10 ? 10 : $scope.readNotifications.length);
+            for (var i = 0; i < totalRead; i++) {
+                if ($scope.readNotifications[i].read == false) {
+                    notifications.setRead($scope.readNotifications[i].id);
+                }
+            }
+        }
     };
 });
