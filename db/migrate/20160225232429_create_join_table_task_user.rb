588d8aa81978fdae5093f621028ba4ad81b63bd7
Ryan Baker <ryan.da.baker@gmail.com>
2016-03-03 05:44:47 -0500

    drops a table before trying to create it again

--- db/migrate/20160225232429_create_join_table_task_user.rb
+++ db/migrate/20160225232429_create_join_table_task_user.rb
@@ -1,5 +1,7 @@
 class CreateJoinTableTaskUser < ActiveRecord::Migration
   def change
+    drop_table :tasks_users
+
     create_join_table :tasks, :users do |t|
        t.index [:task_id, :user_id]
        t.index [:user_id, :task_id]
