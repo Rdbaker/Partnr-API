7ff702a1fa1469d0939fd26be37fb1f8ccaf3a87
Ryan Baker <ryan.da.baker@gmail.com>
2016-03-05 10:37:06 -0500

    upgrades the ruby version (and many other gem versions) as well as updates the setup.sh to work better

--- setup.sh
+++ setup.sh
@@ -3,18 +3,13 @@ sudo apt-get install build-essential zlib1g-dev git-core gnupg2 libpq-dev nodejs
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -L https://get.rvm.io | bash -s stable --ruby
 source /home/vagrant/.rvm/scripts/rvm
-rvm install ruby-2.0.0
+rvm install ruby-2.3.0
 source ~/.profile
-rvm use 2.0.0
+rvm use 2.3.0
 gem install bundler
 bundle install
 gem install mailcatcher
 sudo npm install bower -g
 sudo ln -s /usr/bin/nodejs /usr/bin/node
-
-#### do this part by hand #####
-# sudo -u postgres -i
-# createuser -s vagrant
-# exit
-
-# rake db:setup
+sudo -u postgres -s createuser -s vagrant
+rake db:setup
