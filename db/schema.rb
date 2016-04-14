05fc7a138c1cf3d34868f7b68cdf63a455b05aa5
Ryan Baker <ryan.da.baker@gmail.com>
2016-04-14 12:34:13 -0400

    Adds a migration to add users to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409140508) do
+ActiveRecord::Schema.define(version: 20160414162654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -208,10 +208,12 @@ ActiveRecord::Schema.define(version: 20160409140508) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "bmark_id"
+    t.integer  "user_id"
   end
 
   add_index "tasks", ["bmark_id"], name: "index_tasks_on_bmark_id", using: :btree
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "tasks_users", id: false, force: :cascade do |t|
     t.integer "task_id", null: false
@@ -282,6 +284,7 @@ ActiveRecord::Schema.define(version: 20160409140508) do
   add_foreign_key "skills", "tasks"
   add_foreign_key "tasks", "bmarks"
   add_foreign_key "tasks", "projects"
+  add_foreign_key "tasks", "users"
   add_foreign_key "user_conversations", "conversations"
   add_foreign_key "user_conversations", "users"
   add_foreign_key "users", "notifications"
