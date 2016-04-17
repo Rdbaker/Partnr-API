3fd6776a209492cd498238236da2a8806c1f2ea7
Ryan Baker <ryan.da.baker@gmail.com>
2016-04-16 22:06:46 -0400

    Adds created_at to activity and fixes a localization key issue

--- app/api/v1/entities/activity_data.rb
+++ app/api/v1/entities/activity_data.rb
@@ -5,6 +5,7 @@ module V1::Entities
 
     class AsSearch < AsNested
       expose :id, documentation: { type: "Integer", desc: "The activity id." }
+      expose :created_at, documentation: { type: "Time", desc: "The date and time the activity was created." }, as: :sent_at
       expose :owner, as: :actor, documentation: { type: "UserData (nested)", desc: "The actor for this activity." }, using: UserData::AsNested
       expose :trackable_type, as: :subject_type, documentation: { type: "String", desc: "The class of the subject." }
       expose :text, as: :message
