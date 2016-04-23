2ff6182cd4afcf48fed4ee926c91ead8c7382d32
Ryan Baker <ryan.da.baker@gmail.com>
2016-04-23 15:26:55 -0400

    Updates the setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install build-essential zlib1g-dev git-core gnupg2 libpq-dev nodejs npm postgresql phantomjs
+sudo apt-get install -y build-essential zlib1g-dev git-core gnupg2 libpq-dev nodejs npm postgresql phantomjs
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -L https://get.rvm.io | bash -s stable --ruby
 source /home/vagrant/.rvm/scripts/rvm
@@ -7,9 +7,10 @@ rvm install ruby-2.3.0
 source ~/.profile
 rvm use 2.3.0
 gem install bundler
-bundle install
-gem install mailcatcher
+bundle
+gem install mailcatcher --no-ri --no-doc
 sudo npm install bower -g
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+bower install
 sudo -u postgres -s createuser -s vagrant
 rake db:setup
