785df9c0617eb5838aa03317149df41af4097d61
Tyler Stone <tyler.stone@emc.com>
2016-05-08 11:04:33 -0400

    can create task and assign to user

--- app/assets/javascripts/projects/taskmgr/createTaskCtrl.js
+++ app/assets/javascripts/projects/taskmgr/createTaskCtrl.js
@@ -8,21 +8,44 @@ angular.module('partnr.users.assets').controller('CreateTaskController', functio
 	};
 
 	$scope.loadComplete = false;
-	$scope.loading = false;
+	$scope.formLoading = false;
 	$scope.milestones = [];
+	$scope.users = [];
+	var loadSteps = 2;
+	var loadStepsAchieved = 0;
+
+	var doLoadStep = function() {
+		loadStepsAchieved += 1;
+		if (loadStepsAchieved === loadSteps) {
+			$scope.loadComplete = true;
+		}
+	};
 
 	milestones.listByProject($stateParams.project_id).then(function(result) {
-		$scope.loadComplete = true;
 		$scope.milestones = result.data;
+		doLoadStep();
+	});
+
+	$scope.$parent.getProjectWrapperInfo().then(function(result) {
+		$log.debug(result);
+		$scope.project = result.project;
+		for (var i = 0; i < result.project.roles.length; i++) {
+			var role = result.project.roles[i];
+			if (role.user && role.user.id) {
+				$scope.users.push(role.user);
+			}
+		};
+
+		doLoadStep();
 	});
 
 	var creationFailCallback = function() {
-		$scope.loading = false;
+		$scope.formLoading = false;
 		toaster.error("Task could not be created. Please try again.");
 	};
 
 	$scope.createTask = function() {
-		$scope.loading = true;
+		$scope.formLoading = true;
 
 		if ($scope.task.description === '') {
 			delete $scope.task.description;
@@ -35,7 +58,7 @@ angular.module('partnr.users.assets').controller('CreateTaskController', functio
 		console.log($scope.task);
 
 		tasks.create($scope.task).then(function(result) {
-			$scope.loading = false;
+			$scope.formLoading = false;
 
 			if (result.data.id) {
 				$state.go('project_taskmgr', { project_id: $stateParams.project_id, v: 'task' });
