53237bf3038a770dcfc17cd73e03b022265e4fe9
Tyler Stone <tjs_rocks@yahoo.com>
2016-05-23 23:49:36 -0400

    fixed issue with put requests

--- app/assets/javascripts/projects/taskmgr/taskService.js
+++ app/assets/javascripts/projects/taskmgr/taskService.js
@@ -66,8 +66,25 @@ angular.module('partnr.users.assets').factory('tasks', function($rootScope, $htt
 			$log.debug(task);
 			var taskToUpdate = angular.copy(task);
 
-			if (taskToUpdate.skills.length === 0) delete taskToUpdate.skills;
-			if (taskToUpdate.categories.length === 0) delete taskToUpdate.categories;
+			if (taskToUpdate.skills.length === 0) {
+				delete taskToUpdate.skills;
+			} else if (taskToUpdate.skills[0].id) {
+				var strippedSkills = [];
+				for (var i = 0; i < taskToUpdate.skills.length; i++) {
+					strippedSkills.push(taskToUpdate.skills[i].id);
+				}
+				taskToUpdate.skills = strippedSkills;
+			}
+
+			if (taskToUpdate.categories.length === 0) {
+				delete taskToUpdate.categories;
+			} else if (taskToUpdate.categories[0].id) {
+				var strippedCategories = [];
+				for (var i = 0; i < taskToUpdate.categories.length; i++) {
+					strippedCategories.push(taskToUpdate.categories[i].id);
+				}
+				taskToUpdate.categories = strippedCategories;
+			}
 
 			if (taskToUpdate.milestone && taskToUpdate.milestone.id) {
 				taskToUpdate.milestone = taskToUpdate.milestone.id;
