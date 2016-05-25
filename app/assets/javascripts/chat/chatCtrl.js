9cb8d1766048740ffd9b831f3ca205f0b019a564
timmarschall <services.tim.marschall@outlook.com>
2016-05-25 04:26:27 -0400

    Polling, new chat and minimizing of chat work

--- app/assets/javascripts/chat/chatCtrl.js
+++ app/assets/javascripts/chat/chatCtrl.js
@@ -1,28 +1,52 @@
 angular.module('partnr.users.assets').controller('ChatController', function($scope, $log, users, principal, conversations, $filter, $interval, $rootScope) {
 	$scope.openConversation = {};
-	$scope.users = [];
 	$scope.messageLength = 1000;
 	$scope.currentUserId = principal.getUser().id;
 	$scope.step = 1;
 	$scope.isChatActive = false;
 	$scope.nextButtonTitle = "Start new Conversation";
+	$scope.lessThanOneSelected = true;
+	$scope.isChatWindowOpen = false;
 	var selectedUserIds = [];
+	var pollAllConversationsPromise;
+	var pollOpenConversationPromise;
+	var todayDate = new Date();
 
-	var poll = function poll (callback) {
+	//Filters
+
+	$scope.isNotUserFilter = function isUserFilter (user) {
+		//$log.debug('[CHAT] UserId: ', user.id);
+		//$log.debug('[CHAT] UserId: ', $scope.currentUserId);
+		return (user.id !== $scope.currentUserId);
+	};
+
+	$scope.isAtLeastTwoAndNotCurrentUserFilter = function isAtLeastTwoAndNotCurrentUserFilter (element) {	
+		return !(element.users.length < 2 && !$scope.isNotUserFilter(element.users[0]));
+	};
+
+	$scope.returnDateFilter = function returnDateFilter (date){
+		var messageDate = new Date(date);
+		if (todayDate.setHours(0,0,0,0) == messageDate.setHours(0,0,0,0)) {
+			return 'shortTime';
+		} else {
+			return 'short';
+		}
+	};
+
+	//Polling
+
+	function pollAllConversations (callback) {
 		conversations.list().then(function(result) {
 			$scope.conversations = $filter('filter')(result.data, $scope.isAtLeastTwoAndNotCurrentUserFilter);
 			for (i = 0; i < $scope.conversations.length; i++) {
 				processUserNames($scope.conversations[i]);
+				$scope.conversations[i].date = new Date($scope.conversations[i].last_updated); 
 			}
+			$log.debug('[CHAT] pollAllConversations ', $scope.conversations);
 		});
-	};
-	poll();
-	$interval(poll,$rootScope.pollDuration);
-
-	$scope.$on('$destroy', function() {
-		$log.debug("Cancelling message update requests");
-		$interval.cancel(poll);
-	});
+	}
+	pollAllConversations();
+	pollAllConversationsPromise = $interval(pollAllConversations,$rootScope.pollDuration);
 
 	function processUserNames (conversation) {
 		var displayableUsers = 1;
@@ -39,6 +63,42 @@ angular.module('partnr.users.assets').controller('ChatController', function($sco
 		conversation.non_displayable_name_amount = conversation.users.length - displayableUsers;
 	}
 
+
+	function pollOpenConversation (conversation) {
+		conversations.get(conversation.id).then(function(result) {
+			$scope.openConversation = result.data;
+			$scope.openConversation.namelist = conversation.namelist;
+			$scope.openConversation.non_displayable_name_amount = conversation.non_displayable_name_amount;
+			conversations.changeIsRead(conversation.id, true).then(function(result){
+				conversation.is_read = true;
+			});
+		});
+	}
+
+	//Activation/Deactivation of Chat
+
+	$scope.activateChat = function activateChat (conversation) {
+		$scope.isChatActive = true;
+		conversations.get(conversation.id).then(function(result) {
+			$scope.openConversation = result.data;
+			$scope.openConversation.namelist = conversation.namelist;
+			$scope.openConversation.non_displayable_name_amount = conversation.non_displayable_name_amount;
+			conversations.changeIsRead(conversation.id, true).then(function(result){
+				conversation.is_read = true;
+			});
+		});
+		pollOpenConversationPromise = $interval(pollOpenConversation,5000,0,true, conversation);
+	};
+
+	$scope.deactivateChat = function deactivateChat (conversation) {
+		$scope.isChatActive = false;
+		$scope.openConversation = {};
+		$interval.cancel(pollOpenConversationPromise);
+	};
+
+	
+	//New Chat
+
 	$scope.goStepForward = function goStepForward(){
 		$scope.step = $scope.step + 1;
 		if ($scope.step === 2) {
@@ -54,14 +114,6 @@ angular.module('partnr.users.assets').controller('ChatController', function($sco
 		}
 	};
 
-	$scope.isNextDisabled = function isNextDisabled() {
-		if ($scope.step === 2 && $scope.newMessage.length < 1 && selectedUserIds.length > 0) {
-			return true;
-		} else {
-			return false;
-		}
-	};
-
 	$scope.goStepBack = function goStepBack(){
 		$scope.step = $scope.step - 1;
 		if ($scope.step === 1) {
@@ -70,36 +122,43 @@ angular.module('partnr.users.assets').controller('ChatController', function($sco
 		}
 	};
 
-	$scope.isNotUserFilter = function isUserFilter (user) {
-		//$log.debug('[CHAT] UserId: ', user.id);
-		//$log.debug('[CHAT] UserId: ', $scope.currentUserId);
-		return (user.id !== $scope.currentUserId);
+	$scope.isNextDisabled = function isNextDisabled() {
+		if ($scope.step === 2 && ($scope.newMessage.length < 1 || $scope.lessThanOneSelected)) {
+			return true;
+		} else {
+			return false;
+		}
 	};
 
-	$scope.isAtLeastTwoAndNotCurrentUserFilter = function isAtLeastTwoAndNotCurrentUserFilter (element) {	
-		return !(element.users.length < 2 && !$scope.isNotUserFilter(element.users[0]));
+	$scope.selectUser = function selectUser (user) {
+		var index = selectedUserIds.indexOf(user.id);
+		user.selected ? user.selected = false : user.selected = true;
+		if (index > -1) {
+			selectedUserIds.splice(index, 1);
+
+		} else {
+			selectedUserIds.push(user.id);
+		}
+		(selectedUserIds.length < 1) ? $scope.lessThanOneSelected = true : $scope.lessThanOneSelected = false;
+		$log.debug("[CHAT] selectedUserIds: ", selectedUserIds);
 	};
 
-
-
-	$scope.activateChat = function activateChat (conversation) {
-		$scope.isChatActive = true;
-		conversations.get(conversation.id).then(function(result) {
-			$scope.openConversation = result.data;
-			$scope.openConversation.namelist = conversation.namelist;
-			$scope.openConversation.non_displayable_name_amount = conversation.non_displayable_name_amount;
-			conversations.changeIsRead(conversation.id, true).then(function(result){
-				conversation.is_read = true;
-			});
+	function createConversation() {
+		var newConversation = {
+			users: selectedUserIds,
+			message: $scope.newMessage
+		};
+		conversations.create(newConversation).then(function(result) {
+			$scope.step = 1;
+			$scope.newMessage = "";
+			$scope.nextButtonTitle = "Start new Conversation";
+			$log.debug('[CHAT] newly created conversation', result.data);
+			$scope.activateChat(result.data);
 		});
-	};
 
-	$scope.deactivateChat = function activateChat (conversation) {
-		$scope.isChatActive = false;
-		conversations.get(conversation.id).then(function(result) {
-			$scope.openConversation = result.data;
-		});
-	};
+	}
+
+	//Send Message
 
 	$scope.sendMessage= function(event){
 		if (event.keyCode === 13) {
@@ -109,16 +168,6 @@ angular.module('partnr.users.assets').controller('ChatController', function($sco
 			conversations.get($scope.openConversation.id).then(function(result) {
 				$scope.openConversation = result.data;
 			});
-		}
-	};
-
-	$scope.returnDateFilter = function returnDateFilter (date){
-		var todayDate = new Date();
-		var messageDate = new Date(date);
-		if (todayDate.setHours(0,0,0,0) == messageDate.setHours(0,0,0,0)) {
-			return 'shortTime';
-		} else {
-			return 'short';
 		}
 	};
 
@@ -139,31 +188,4 @@ angular.module('partnr.users.assets').controller('ChatController', function($sco
 			});
 		}
 	};
-
-	$scope.selectUser = function selectUser (user) {
-		var index = selectedUserIds.indexOf(user.id);
-		user.selected ? user.selected = false : user.selected = true;
-		if (index > -1) {
-			selectedUserIds.splice(index, 1);
-
-		} else {
-			selectedUserIds.push(user.id);
-		}
-		$log.debug("[CHAT] selectedUserIds: ", selectedUserIds);
-	};
-
-	function createConversation() {
-		var newConversation = {
-			users: selectedUserIds,
-			message: $scope.newMessage
-		};
-		conversations.create(newConversation).then(function(result) {
-			$scope.openConversation = result.data;
-			$scope.step = 1;
-			$scope.nextButtonTitle = "Start new Conversation";
-			$scope.newMessage = "";
-			//$scope.activateChat(result)
-		});
-
-	}
 });
