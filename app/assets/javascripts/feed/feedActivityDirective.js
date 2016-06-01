bbe237498517471e31c38eb921b06c1cafe097fd
Ryan Baker <ryan.da.baker@gmail.com>
2016-05-31 22:04:30 -0400

    Solves a bug for an edge case.

--- app/assets/javascripts/feed/feedActivityDirective.js
+++ app/assets/javascripts/feed/feedActivityDirective.js
@@ -12,8 +12,12 @@ angular.module('partnr.feed').directive('feedActivity', function($rootScope, $st
         var subject_type = a.subject_type.toLowerCase();
         if(subject_type === "bmark")
           subject_type = "milestone";
-
-        routeUtils.resolveEntityLinkAndGo(a.subject[subject_type].links.self, a);
+        if(subject_type === "role" || subject_type === "comment") {
+          // hard coded edge case for now, because "role" doesn't have it's own state and should resolve to project
+          $state.go('project', { project_id: a.subject.project.id });
+        } else {
+          routeUtils.resolveEntityLinkAndGo(a.subject[subject_type].links.self, a);
+        }
       };
     }
   };
