cb7b44fb1c74ff04550a365638b1e53658513354
Tyler Stone <tyler.stone@emc.com>
2016-06-04 17:14:03 -0400

    still wip: trying to make profile page more similar to project page with tabbed layout

--- app/assets/javascripts/user/profile/profileCtrl.js
+++ app/assets/javascripts/user/profile/profileCtrl.js
@@ -1,10 +1,13 @@
-angular.module('partnr.users.assets').controller('ProfileController', function($scope, $state, $stateParams, $log, toaster, users) {
+angular.module('partnr.users.assets').controller('ProfileController', function($scope, $rootScope, $state, $stateParams, $log, toaster, users) {
 	$scope.loadComplete = false;
 	$scope.user = null;
+	$scope.userCategories = $rootScope.categories;
 
 	users.get($stateParams.id).then(function(result) {
 		$log.debug(result.data);
 		$scope.user = result.data;
 		$scope.loadComplete = true;
+
+		$log.debug(result.data);
 	});
 });
