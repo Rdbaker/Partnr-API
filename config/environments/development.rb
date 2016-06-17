0978ae0de03ac2fa25f93b4c7850b566e67344e4
Ryan Baker <rdbaker@kyruus.com>
2016-06-17 13:37:53 +0000

    development.rb edited online with Bitbucket

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
     config.action_mailer.default_url_options = { :host => 'localhost:3000' }
     config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   else
-    Rails.application.config.host = 'http://dev.partnr.org'
-    config.action_mailer.default_url_options = { :host => 'dev.partnr.org' }
+    Rails.application.config.host = 'http://dev.partnr-up.com'
+    config.action_mailer.default_url_options = { :host => 'dev.partnr-up.com' }
     config.action_mailer.smtp_settings = {
       :user_name => 'partnremailer',
       :password => 'P4rtnrS3nds3m4ilsN0w',
