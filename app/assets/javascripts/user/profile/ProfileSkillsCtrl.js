e981bcb550e0abaeb285ef3a9645c0b022249008
Tim Marschall <Tim Marschall>
2016-08-05 00:33:03 -0400

    better display of skills and projects

--- app/assets/javascripts/user/profile/ProfileSkillsCtrl.js
+++ app/assets/javascripts/user/profile/ProfileSkillsCtrl.js
@@ -2,14 +2,9 @@ angular.module('partnr.users.assets').controller('ProfileSkillsController', func
 	$scope.loadComplete = false;
 	$scope.user = null;
 	$scope.categorySkillMap = [];
-	$scope.totalScore = 0;
 
 	$scope.$parent.getProfileWrapperInfo().then(function(result) {
 		$scope.user = result;
-
-		for (var i = 0; i < $scope.user.skillscore.scored.categories.length; i++) {
-			$scope.totalScore += $scope.user.skillscore.scored.categories[i].score;
-		}
 
 		for (var i = 0; i < $scope.user.skillscore.scored.skills.length; i++) {
 			var currentSkill = $scope.user.skillscore.scored.skills[i].skill;
