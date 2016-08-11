87bf18964140594e38e90492cd7e683fc11ff2a0
Ryan Baker <ryan.da.baker@gmail.com>
2016-08-11 08:23:25 -0400

    updates a migration to be conditional.

--- db/migrate/20160225232429_create_join_table_task_user.rb
+++ db/migrate/20160225232429_create_join_table_task_user.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableTaskUser < ActiveRecord::Migration
   def change
-    drop_table :tasks_users
+    drop_table 'tasks_users' if ActiveRecord::Base.connection.table_exists? 'tasks_users'
 
     create_join_table :tasks, :users do |t|
        t.index [:task_id, :user_id]
