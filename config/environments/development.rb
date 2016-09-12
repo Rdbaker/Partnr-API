50de8340d5013e262a6156fb0a8edc876b680a11
Ryan Baker <ryan.da.baker@gmail.com>
2016-09-12 18:13:01 -0400

    More updates.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.middleware.insert_before Warden::Manager, Rack::Cors do
     allow do
       origins '*.partnr-up.com'
-      resource '/api/*', :headers => :any, :methods => [:get, :post, :options, :put, :patch]
+      resource '/api/*', :headers => :any, :methods => [:get, :post, :options, :put, :patch], :credentials => true
     end
   end
 
