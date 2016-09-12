a65305ec565797898fcfd3e220730acaf15909a7
Ryan Baker <ryan.da.baker@gmail.com>
2016-09-12 18:12:13 -0400

    Removes a wildcard

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -25,9 +25,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def set_headers
-    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS, HEAD'
-    headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
