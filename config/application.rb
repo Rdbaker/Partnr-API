fbebbf3d24e3dbbaf028db032e74c7b4ca9e3075
Ryan Baker <ryan.da.baker@gmail.com>
2016-09-12 19:08:13 -0400

    Updates cors.

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module Partnr
     # specify middleware order so CORS is before warden (devise)
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://app.partnr-up.com', 'http://dev.partnr-up.com'
+        origin 'partnr-up.com'
         resource '/api/', :headers => :any, :methods => [:get, :post, :options, :put, :patch], :credentials => true, :max_age => 600
       end
     end
