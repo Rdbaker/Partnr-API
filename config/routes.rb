06e1c91328fd4c20f62bc9f555b9d8f6fb4f7399
Ryan Baker <ryan.da.baker@gmail.com>
2016-09-13 19:42:27 -0400

    Fixes a protocol issue.

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
   root to: 'application#angular'
 
   if Rails.env.production?
-    get '*path', to: redirect(:host => "https://app.partnr-up.com.s3-website-us-west-2.amazonaws.com", :port => 443)
+    get '*path', to: redirect(:host => "http://app.partnr-up.com.s3-website-us-west-2.amazonaws.com", :port => 80)
   else
-    get '*path', to: redirect(:host => "https://dev.partnr-up.com.s3-website-us-west-2.amazonaws.com", :port => 443)
+    get '*path', to: redirect(:host => "http://dev.partnr-up.com.s3-website-us-west-2.amazonaws.com", :port => 80)
   end
 
 end
