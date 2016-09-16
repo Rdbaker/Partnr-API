01e78ce34aa4282d67b432f3fee7440dd77e8579
Ryan Baker <ryan.da.baker@gmail.com>
2016-09-15 22:05:39 -0400

    Quick redirect fix for prod env.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   root to: 'application#angular'
 
   if Rails.env.production?
-    get '*path', to: redirect(:host => "http://app.partnr-up.com.s3-website-us-west-2.amazonaws.com", :port => 80)
+    get '*path', to: redirect(:host => "https://s3-us-west-2.amazonaws.com/app.partnr-up.com", :port => 443)
   else
     get '*path', to: redirect(:host => "http://dev.partnr-up.com.s3-website-us-west-2.amazonaws.com", :port => 80)
   end
